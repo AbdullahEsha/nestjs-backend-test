fa86946f3138f452d8524295b3eda3bea8f809c8
AbdullahIsha <shahariaresha@gmail.com>
2023-06-15 17:12:38 +0600

    update

---
+++ src/cats.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+  @Get()
+  findAll(@Req() request: Request): string {
+    return this.catsService.getCats();
+  }
+}
